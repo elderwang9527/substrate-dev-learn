2daa44fb56629a0abda75e72ce6bf3b4a1ed8ae5
playdog-io <cqdxcs3@163.com>
2021-06-18 09:15:32 +0000

    Write a Pallet from Scratch.2

---
+++ pallets/template/src/lib.rs
@@ -0,0 +1,34 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+// Re-export pallet items so that they can be accessed from the crate namespace.
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
+    use sp_std::vec::Vec; // Step 3.1 will include this in `Cargo.toml`
+
+    /// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+	  // <-- Step 2. code block will replace this.
+
+    #[pallet::event]   // <-- Step 3. code block will replace this.
+    
+    #[pallet::error]   // <-- Step 4. code block will replace this.
+    
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+    
+    #[pallet::storage] // <-- Step 5. code block will replace this.
+    
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    
+    #[pallet::call]   // <-- Step 6. code block will replace this.
+}
